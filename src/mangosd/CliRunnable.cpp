66a4210d934e2f96c629d1342229c80f4c611831
KeRo99 <motrilwireless@gmail.com>
2010-04-22 22:18:07 +0200

    Revert "Temp Linux Compile Fix"
    This reverts commit 93f66e7b110b2384a3e1b4cbb307f9960c8b56ec.

--- src/mangosd/CliRunnable.cpp
+++ src/mangosd/CliRunnable.cpp
@@ -317,9 +317,7 @@ bool ChatHandler::HandleCharacterDeletedRestoreCommand(const char* args)
     std::string newCharName;
     uint32 newAccount = 0;
 
-    std::istringstream parm(args);
-	
-	parm >> searchString >> newCharName >> newAccount;
+    std::istringstream(args) >> searchString >> newCharName >> newAccount;
 
     DeletedInfoList foundList;
     if (!GetDeletedCharacterInfoList(foundList, searchString))

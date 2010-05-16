b266cf16e8b1bda203e76567d8c4c383c1cee6df
KeRo99 <motrilwireless@gmail.com>
2010-05-16 02:32:41 +0200

    Revert "[K37] Overkill after vanish"
    This reverts commit 3869345a810c6a3ca2b9357aa8bb4477f8a56c92.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -6560,9 +6560,6 @@ void Spell::EffectSanctuary(SpellEffectIndex /*eff_idx*/)
     if(m_spellInfo->SpellFamilyName == SPELLFAMILY_ROGUE && (m_spellInfo->SpellFamilyFlags & SPELLFAMILYFLAG_ROGUE_VANISH))
     {
         ((Player *)m_caster)->RemoveSpellsCausingAura(SPELL_AURA_MOD_ROOT);
-        // Overkill 
-        if(((Player*)m_caster)->HasSpell(58426))
-            m_caster->CastSpell(m_caster, 58427, true);
     }
 }
 

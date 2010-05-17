2a1586e233480f599580a444224595a1327ace26
selector <limurchick@gmail.com>
2010-05-17 13:28:01 +0300

    [C21] Mana Tide Totem inherits 10% of owner health
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -7112,8 +7112,16 @@ void Spell::DoSummonTotem(SpellEffectIndex eff_idx, uint8 slot_dbc)
 
     if (damage)                                             // if not spell info, DB values used
     {
-        pTotem->SetMaxHealth(damage);
-        pTotem->SetHealth(damage);
+        if (pTotem->GetEntry() == 10467) // Mana Tide inherits 10% of owner health
+        {
+            pTotem->SetMaxHealth(m_caster->GetMaxHealth()*0.1);
+            pTotem->SetHealth(m_caster->GetMaxHealth()*0.1);
+        }
+        else
+        {
+            pTotem->SetMaxHealth(damage);
+            pTotem->SetHealth(damage);
+        }
     }
 
     pTotem->SetUInt32Value(UNIT_CREATED_BY_SPELL, m_spellInfo->Id);

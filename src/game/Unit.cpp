4cbc0ff1e7ab225ca0d2e0797d7cb884faa77907
selector <limurchick@gmail.com>
2010-07-08 20:57:13 +0300

    [C68] Crashfix, comment one ASSERT
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -12273,7 +12273,7 @@ bool Unit::SelectHostileTarget()
     //next-victim-selection algorithm and evade mode are called
     //threat list sorting etc.
 
-    ASSERT(GetTypeId()== TYPEID_UNIT);
+    //ASSERT(GetTypeId()== TYPEID_UNIT);
 
     if (!this->isAlive())
         return false;

35e1f1c5281fe451a69af2de5642f82ce1423d0d
Wowka321 <galaxys@inbox.ru>
2010-07-15 15:51:22 +0300

    [C103] GO-Loot rules
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/Object.h
+++ src/game/Object.h
@@ -69,6 +69,7 @@ class WorldSession;
 class Creature;
 class GameObject;
 class Player;
+class Group;
 class Unit;
 class Map;
 class UpdateMask;
@@ -500,6 +501,11 @@ class MANGOS_DLL_SPEC WorldObject : public Object
         void AddToClientUpdateList();
         void RemoveFromClientUpdateList();
         void BuildUpdateData(UpdateDataMapType &);
+
+        uint32 m_groupLootTimer;                            // (msecs)timer used for group loot
+        uint32 m_groupLootId;                               // used to find group which is looting corpse
+        void StopGroupLoot();
+        void StartGroupLoot(Group* group, uint32 timer);
 
         Creature* SummonCreature(uint32 id, float x, float y, float z, float ang,TempSummonType spwtype,uint32 despwtime);
         GameObject* SummonGameobject(uint32 id, float x, float y, float z, float angle, uint32 despwtime);

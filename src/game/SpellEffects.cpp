5d465da3b0ab8cd80b3aa6725973e68083b08ac9
x3n <MENUS-IV@.(none)>
2010-08-12 21:04:59 +0200

    [C106] Added support for spell 43972 and 43375 (Quest 11306)
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -6480,7 +6480,19 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                     m_caster->CastSpell(unitTarget, 71480, true);
                     return;
                 }
-            }
+				case 43972:
+				case 43375:		// Mixing Blood for Quest 11306 
+                {
+					switch(urand(0, 2))
+					{
+						case 0 : m_caster->CastSpell(m_caster, 43378, true); break;
+						case 1 : m_caster->CastSpell(m_caster, 43376, true); break;
+						case 2 : m_caster->CastSpell(m_caster, 43377, true); break;
+						case 3 : m_caster->CastSpell(m_caster, 43970, true); break;
+					}
+				}
+                break;	
+			}
             break;
         }
         case SPELLFAMILY_WARLOCK:
